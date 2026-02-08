654e6aaa79a43d931186a0c5d98d565b3036141e
Muhammad Mahad <160241414+Mahad737@users.noreply.github.com>
2026-02-08 20:44:35 +0500

    Refactor service worker for cache management

--- service-woker.js
+++ service-woker.js
@@ -1,23 +1,10 @@
-const cacheName = 'budget-tracker-v1';
-const assets = [
-  './',
-  './index.html',
-];
+const cacheName = 'budget-v1';
+const assets = ['./', './index.html'];
 
-// Files ko save karna
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Offline hone par cache se file dikhana
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
